76be2f18ffe6975e53b3cc9f63622aa5bb9f6b4a
Alexey Nesterov <alexey.nesterov@live.com>
2017-12-16 21:03:34 +0000

    Add Maven Central credentials into build container
    Skip signing for now

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,8 +2,13 @@
 
 set -e -x
 
+mkdir ~/.gradle/
+touch ~/.gradle/gradle.properties
+echo "ossrhUser=${OSSRHUSER}" >> ~/.gradle/gradle.properties
+echo "ossrhPassword=${OSSRHPASSWORD}" >> ~/.gradle/gradle.properties
+
 cd sources
 
-./gradlew :dataj-core:build
+./gradlew :dataj-core:build -x signArchives
 
 cp dataj-core/build/libs/* ../build
